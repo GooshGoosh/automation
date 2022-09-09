a4c6807dba322d88ba14b6850a0eb9e79f72c9d4
Dirty Dan <dirtydan@mypc>
2022-09-09 18:50:27 -0500

    Add 3 new functions to test the systems network
    The function check_localhost uses the gethostbyname function provided by
    the socket module to check if the 'localhost' host name translates to
    the proper IPv4 address of '127.0.0.1'. The function check_internet
    takes 3 parameters (IPv4 address, port number, and an integer) as a host,
    port, and timeout value, respectively. check_internet makes a connection
    attempt for the IPv4 address of '8.8.8.8' (public Google DNS server) on
    port 53 (DNS) to test if the system can reach outside the local network.
    The function check_DNS takes a single parameter (str) as a host name
    which then uses the gethostbyname function provided by the socket module
    to check if 'google-public-dns-a.google.com' (public Google DNS server)
    correctly translates to the IPv4 address of '8.8.8.8'.

--- checkSystemResources.py
+++ checkSystemResources.py
@@ -7,6 +7,7 @@
 import os
 import shutil
 import psutil
+import socket
 
 
 def check_disk_usage(disk):
@@ -41,6 +42,38 @@ def check_memory_usage():
         print('Memory Check: OK')
 
 
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    if localhost == "127.0.0.1":
+        print('localhost: 127.0.0.1')
+    else:
+        print('localhost: Failed')
+        
+        
+def check_internet(host='8.8.8.8', port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        print('Internet Check: OK')
+    except socket.error as ex:
+        #print(ex)
+        print('Internet Check: Failed')
+
+
+def check_DNS(host='google-public-dns-a.google.com', timeout=3):
+    socket.setdefaulttimeout(timeout)
+    googleDNS = socket.gethostbyname(host)
+    if googleDNS == '8.8.8.8':
+        print('DNS Check: OK')
+    else:
+        print('DNS Check: Failed')
+
+
 def check_reboot_required():
     if os.path.exists('/var/run/reboot-required'):      # Check if the 'reboot-required' file exists on the system
         print('REBOOT REQUIRED')                        # and print a message depending on if it exists or not.
@@ -54,10 +87,18 @@ def check_reboot_required():
 
 print('\nChecking Disk...\n')
 check_disk_usage('/')
+
 print('\nChecking CPU...\n')
 check_cpu_usage()
+
 print('\nChecking Memory...\n')
 check_memory_usage()
+
+print('\nChecking Internet Connection...\n')
+check_localhost()   
+check_internet()
+check_DNS()
+
 print('\nChecking Reboot...\n')
 check_reboot_required()
 print('\nChecks finished\nExiting...')
