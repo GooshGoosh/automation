890b3496baa179511652fba3095cd933875ab5eb
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2022-09-21 21:27:38 -0500

    Add main function
    Added a main function to contain the code that is run in the program.
    Also included the __name__ variable to better control how the program
    gets handled when executed as a script or imported.

--- healthChecks.py
+++ healthChecks.py
@@ -10,15 +10,21 @@ def check_disk_usage(disk):
 	free = du.free / du.total * 100
 	return free > 20
 
+
 def check_cpu_usage():
 	usage = psutil.cpu_percent(1)
 	return usage < 75
 
 
-if not check_disk_usage("/") or not check_cpu_usage():
-	print("ERROR!")
-elif check_localhost() and check_connectivity():
-	print("Everything ok")
-else:
-	print("Network checks failed")
-
+def main():
+    if not check_disk_usage("/") or not check_cpu_usage():
+        print("ERROR!")
+    elif check_localhost() and check_connectivity():
+        print("Everything ok")
+    else:
+        print("Network checks failed")
+  
+  
+if __name__ == "__main__":
+    main()
+    
