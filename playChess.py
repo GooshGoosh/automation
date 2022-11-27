023203e6e11c0ce6cf08d3f21d71f8d0d0b8426a
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2022-11-26 18:01:52 -0600

    Change chess board space names
    This edit focuses on changing the chess board space names used in the
    program such as changing '7a' to 'a7'. The edit also changes the layout
    of the printed chess board so that the white pieces are at the bottom
    and the row numbering is changed so that it starts at 1 from the white
    side of the board.

--- playChess.py
+++ playChess.py
@@ -9,13 +9,13 @@ created.
 The players will take turns and on each turn they will be given two options: move a piece on a specified space or save and exit the game. If the player chooses to move
 a piece on a specified space, they will then be given a prompt to select a space (piece) that they would like to move and then to choose a space that they would
 like to move their piece to. Players will have to select the space that they would like to interact with that contains the piece that they would like to move.
-e.g. if the white queen is on space 3h, the player would type in '3h' and then type in the space that they would like to move it to such as '6h'.
+e.g. if the white queen is on space h3, the player would type in 'h3' and then type in the space that they would like to move it to such as 'h6'.
 If the player chooses the option to save and quit, the program will save the current board configuration and number of moves for each player to a json file.
 The current configuration of the chess board will be printed out to the screen after each player's turn.
 
 WARNING: This program assumes that the player's know how to play chess and intend to play it fairly without exploiting the program. This program does not check if
 any moves made by either player are legal or if the correct player is moving their intended piece (i.e. the black player moving the white pieces). The program only
-ensures that the players interact with legal spaces on the chess board and do not try to move pieces to and from invalid board spaces such as 7z or 23r.
+ensures that the players interact with legal spaces on the chess board and do not try to move pieces to and from invalid board spaces such as z7 or r23.
 '''
 
 import json
@@ -49,14 +49,14 @@ def loadBoard():
         time.sleep(1)
         
         board = {
-            '1a': 'wr', '1b': 'wk', '1c': 'wb', '1d': 'WK', '1e': 'wq', '1f': 'wb', '1g': 'wk', '1h': 'wr',
-            '2a': 'wp', '2b': 'wp', '2c': 'wp', '2d': 'wp', '2e': 'wp', '2f': 'wp', '2g': 'wp', '2h': 'wp',
-            '3a': '  ', '3b': '  ', '3c': '  ', '3d': '  ', '3e': '  ', '3f': '  ', '3g': '  ', '3h': '  ',
-            '4a': '  ', '4b': '  ', '4c': '  ', '4d': '  ', '4e': '  ', '4f': '  ', '4g': '  ', '4h': '  ',
-            '5a': '  ', '5b': '  ', '5c': '  ', '5d': '  ', '5e': '  ', '5f': '  ', '5g': '  ', '5h': '  ',
-            '6a': '  ', '6b': '  ', '6c': '  ', '6d': '  ', '6e': '  ', '6f': '  ', '6g': '  ', '6h': '  ',
-            '7a': 'bp', '7b': 'bp', '7c': 'bp', '7d': 'bp', '7e': 'bp', '7f': 'bp', '7g': 'bp', '7h': 'bp',
-            '8a': 'br', '8b': 'bk', '8c': 'bb', '8d': 'BK', '8e': 'bq', '8f': 'bb', '8g': 'bk', '8h': 'br'
+            'a1': 'wr', 'b1': 'wk', 'c1': 'wb', 'd1': 'WK', 'e1': 'wq', 'f1': 'wb', 'g1': 'wk', 'h1': 'wr',
+            'a2': 'wp', 'b2': 'wp', 'c2': 'wp', 'd2': 'wp', 'e2': 'wp', 'f2': 'wp', 'g2': 'wp', 'h2': 'wp',
+            'a3': '  ', 'b3': '  ', 'c3': '  ', 'd3': '  ', 'e3': '  ', 'f3': '  ', 'g3': '  ', 'h3': '  ',
+            'a4': '  ', 'b4': '  ', 'c4': '  ', 'd4': '  ', 'e4': '  ', 'f4': '  ', 'g4': '  ', 'h4': '  ',
+            'a5': '  ', 'b5': '  ', 'c5': '  ', 'd5': '  ', 'e5': '  ', 'f5': '  ', 'g5': '  ', 'h5': '  ',
+            'a6': '  ', 'b6': '  ', 'c6': '  ', 'd6': '  ', 'e6': '  ', 'f6': '  ', 'g6': '  ', 'h6': '  ',
+            'a7': 'bp', 'b7': 'bp', 'c7': 'bp', 'd7': 'bp', 'e7': 'bp', 'f7': 'bp', 'g7': 'bp', 'h7': 'bp',
+            'a8': 'br', 'b8': 'bk', 'c8': 'bb', 'd8': 'BK', 'e8': 'bq', 'f8': 'bb', 'g8': 'bk', 'h8': 'br'
         }   # Dictionary for a new, freshly set up chess board.
         
         moves = {'playerWhite': 0, 'playerBlack': 0}
@@ -111,6 +111,7 @@ def saveBoard(board, moves, removedPieces, player1, player2):
     
     
 def printBoard(board, removedPieces, player1, player2):
+    rowNum = 8
     os.system('cls' if os.name == 'nt' else 'clear')
     print('White: {}'.format(player1))
     print('Black: {}'.format(player2))
@@ -118,22 +119,22 @@ def printBoard(board, removedPieces, player1, player2):
     
     # Create a list of dictionaries for each row of chess board spaces.
     listOfSpaces = [
-        {   '1a': board['1a'], '1b': board['1b'], '1c': board['1c'], '1d': board['1d'],
-            '1e': board['1e'], '1f': board['1f'], '1g': board['1g'], '1h': board['1h']},
-        {   '2a': board['2a'], '2b': board['2b'], '2c': board['2c'], '2d': board['2d'],
-            '2e': board['2e'], '2f': board['2f'], '2g': board['2g'], '2h': board['2h']},
-        {   '3a': board['3a'], '3b': board['3b'], '3c': board['3c'], '3d': board['3d'],
-            '3e': board['3e'], '3f': board['3f'], '3g': board['3g'], '3h': board['3h']},
-        {   '4a': board['4a'], '4b': board['4b'], '4c': board['4c'], '4d': board['4d'],
-            '4e': board['4e'], '4f': board['4f'], '4g': board['4g'], '4h': board['4h']},
-        {   '5a': board['5a'], '5b': board['5b'], '5c': board['5c'], '5d': board['5d'],
-            '5e': board['5e'], '5f': board['5f'], '5g': board['5g'], '5h': board['5h']},
-        {   '6a': board['6a'], '6b': board['6b'], '6c': board['6c'], '6d': board['6d'],
-            '6e': board['6e'], '6f': board['6f'], '6g': board['6g'], '6h': board['6h']},
-        {   '7a': board['7a'], '7b': board['7b'], '7c': board['7c'], '7d': board['7d'],
-            '7e': board['7e'], '7f': board['7f'], '7g': board['7g'], '7h': board['7h']},
-        {   '8a': board['8a'], '8b': board['8b'], '8c': board['8c'], '8d': board['8d'],
-            '8e': board['8e'], '8f': board['8f'], '8g': board['8g'], '8h': board['8h']}       
+        {   'a8': board['a8'], 'b8': board['b8'], 'c8': board['c8'], 'd8': board['d8'],
+            'e8': board['e8'], 'f8': board['f8'], 'g8': board['g8'], 'h8': board['h8']},
+        {   'a7': board['a7'], 'b7': board['b7'], 'c7': board['c7'], 'd7': board['d7'],
+            'e7': board['e7'], 'f7': board['f7'], 'g7': board['g7'], 'h7': board['h7']},
+        {   'a6': board['a6'], 'b6': board['b6'], 'c6': board['c6'], 'd6': board['d6'],
+            'e6': board['e6'], 'f6': board['f6'], 'g6': board['g6'], 'h6': board['h6']},
+        {   'a5': board['a5'], 'b5': board['b5'], 'c5': board['c5'], 'd5': board['d5'],
+            'e5': board['e5'], 'f5': board['f5'], 'g5': board['g5'], 'h5': board['h5']},
+        {   'a4': board['a4'], 'b4': board['b4'], 'c4': board['c4'], 'd4': board['d4'],
+            'e4': board['e4'], 'f4': board['f4'], 'g4': board['g4'], 'h4': board['h4']}, 
+        {   'a3': board['a3'], 'b3': board['b3'], 'c3': board['c3'], 'd3': board['d3'],
+            'e3': board['e3'], 'f3': board['f3'], 'g3': board['g3'], 'h3': board['h3']}, 
+        {   'a2': board['a2'], 'b2': board['b2'], 'c2': board['c2'], 'd2': board['d2'],
+            'e2': board['e2'], 'f2': board['f2'], 'g2': board['g2'], 'h2': board['h2']},
+        {   'a1': board['a1'], 'b1': board['b1'], 'c1': board['c1'], 'd1': board['d1'],
+            'e1': board['e1'], 'f1': board['f1'], 'g1': board['g1'], 'h1': board['h1']}     
     ]
     
     # Print the column headers for the chess board.
@@ -143,7 +144,8 @@ def printBoard(board, removedPieces, player1, player2):
     # Loop through the list and each dictionary to print the piece that occupies the space
     # or blank if there is no piece present in the space.
     for dict in listOfSpaces:
-        print('{} | '.format(int((listOfSpaces.index(dict))) + 1), end='')      # Print the row headers.
+        print('{} | '.format(rowNum), end='')      # Print the row headers.
+        rowNum -= 1
         for k, v in dict.items():
             print('{} | '.format(v), end='')
         print('\n' + ('-' * 43))
@@ -166,9 +168,9 @@ def chessTurns(chessBoard, playerMoves, removedPieces, playerWhite, playerBlack)
             
             if move == 'Move Piece':
                 while startingSpace not in chessBoard.keys():
-                    startingSpace = pyip.inputStr(prompt='\nType the space that the piece you would like to move resides in (e.g. 2a) > ')
+                    startingSpace = pyip.inputStr(prompt='\nType the space that the piece you would like to move resides in (e.g. a2) > ')
                 while destinationSpace not in chessBoard.keys() and destinationSpace != startingSpace:
-                    destinationSpace = pyip.inputStr(prompt='\nType the space that you would like to move the piece to (e.g. 3a) > ')
+                    destinationSpace = pyip.inputStr(prompt='\nType the space that you would like to move the piece to (e.g. a3) > ')
                 if chessBoard[destinationSpace] != '  ':
                     removedPieces['removedBlack'].append(chessBoard[destinationSpace])
                 chessBoard[destinationSpace] = chessBoard[startingSpace]
@@ -187,9 +189,9 @@ def chessTurns(chessBoard, playerMoves, removedPieces, playerWhite, playerBlack)
             
             if move == 'Move Piece':
                 while startingSpace not in chessBoard.keys():
-                    startingSpace = pyip.inputStr(prompt='\nType the space that the piece you would like to move resides in (e.g. 2a) > ')
+                    startingSpace = pyip.inputStr(prompt='\nType the space that the piece you would like to move resides in (e.g. a7) > ')
                 while destinationSpace not in chessBoard.keys() and destinationSpace != startingSpace:
-                    destinationSpace = pyip.inputStr(prompt='\nType the space that you would like to move the piece to (e.g. 3a) > ')
+                    destinationSpace = pyip.inputStr(prompt='\nType the space that you would like to move the piece to (e.g. a6) > ')
                 if chessBoard[destinationSpace] != '  ':
                     removedPieces['removedWhite'].append(chessBoard[destinationSpace])
                 chessBoard[destinationSpace] = chessBoard[startingSpace]
@@ -201,7 +203,7 @@ def chessTurns(chessBoard, playerMoves, removedPieces, playerWhite, playerBlack)
         else:
             print('\nERROR: Black has more moves taken than White! Exiting program with an error!')
             sys.exit(1)
-                
+                             
                 
     
 def main():
