87b8910d0a27d053a5015141af46b93c73ae51dc
GooshGoosh <113269398+GooshGoosh@users.noreply.github.com>
2023-03-12 19:07:05 -0500

    Add backup-files.sh bash script
    The backup-files.sh bash script is used to run the backupFile,py script.
    The backup-files.sh script takes no arguments and checks if the backupFile.py
    script (1) exists in the path specified in the shell script, (2) is a regular file
    and not a directory or a device file, and (3) if the backupFile.py script has execute
    permissions for the current user.

---
+++ backup-files.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# backup-files.sh - Runs the backupFile.py script on any file specified.
+# This allows the backupFile.py script to remain small while still allowing for
+# multiple files to be backed up. This also allows for the backupFile.py script
+# to remain independent of the OS that it is running on.
+
+
+BACKUPSCRIPT=/home/dirtypi/scripts/automation/backupFile.py 
+
+echo -e "\nChecking for backup script..."
+
+if [[ ! -e $BACKUPSCRIPT ]]; then
+  echo -e "\nBackup script not found! Exiting..."
+  exit
+elif [[ ! -f $BACKUPSCRIPT ]]; then
+  echo -e "\nBackup script file location is either a directory or a device file! Exiting"
+  exit 
+elif [[ ! -x $BACKUPSCRIPT ]]; then
+  echo -e "\nBackup script does not have execute permissions for current user! Exiting..."
+  exit
+else
+  $BACKUPSCRIPT /home/dirtypi/test.txt 
+fi 
+
